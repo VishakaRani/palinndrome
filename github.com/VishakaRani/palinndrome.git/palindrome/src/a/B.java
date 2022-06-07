361e8b19cc340b12be9394e26df24603908b7416
VISHAKA RANI <VISHAKA RANI@user>
2022-06-08 00:36:27 +0530

    commit

---
+++ github.com/VishakaRani/palinndrome.git/palindrome/src/a/B.java
@@ -0,0 +1,35 @@
+package a;
+
+public class B 
+{
+public static void main(String[]args)
+{
+	String s1="Dr.AIT";
+	System.out.println("The length of s1 id:"+s1.length());
+	System.out.println("The Index of T is:"+s1.indexOf('T'));
+	
+	String s2="Monday";
+	String s3=s2.substring(3,6);
+	String s8=s2.substring(3);
+	System.out.println(s3);
+	System.out.println(s8);
+	
+	String s4="   hi     ";
+	String s5=s4.trim();
+	System.out.println(s5);
+	
+	String sentence="HI WELCOME TO JAVA CLASS";
+	char ch[]=sentence.toCharArray();
+	
+	for(int i=0;i<sentence.length();i++)
+	{
+		System.out.println(ch[i]);
+	}
+	System.out.println(sentence.length());
+	
+	String s6="java";
+	System.out.println(s6);
+	String s7=s6.toUpperCase();
+	System.out.println(s7);
+}
+}
